a77950eda32182da026fbda4515dd7cb39a7c058
Arthur Oliveira (Retail) <AR40044527@wipro.com>
2019-12-22 18:31:14 -0300

    Atividade Angular

---
+++ spring-pokedex/src/main/java/com/wipro/springpokedex/SpringPokedexApplication.java
@@ -0,0 +1,13 @@
+package com.wipro.springpokedex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPokedexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPokedexApplication.class, args);
+	}
+
+}
